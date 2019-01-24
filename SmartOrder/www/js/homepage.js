f9eef36fc13ddf9bd8363e5d48d2a9ccdac4c302
Tommaso Carraro <tommasocarraro@libero.it>
2019-01-24 14:57:11 +0100

    quasi finito

--- SmartOrder/www/js/homepage.js
+++ SmartOrder/www/js/homepage.js
@@ -34,13 +34,15 @@ function compute(xhttp) {
     document.getElementById("name").innerHTML = risp.nome;
     document.getElementById("code").innerHTML = codiceAzienda;
     var div = document.getElementById("articleList");
+    var total = 0.0;
     if(risp.articoli.length == 0){
         var p = document.createElement("p");
         p.setAttribute("class","cart-empty");
         var text = document.createTextNode("Nessun articolo in carrello");
         p.appendChild(text);
         div.appendChild(p);
-    }else{
+        document.getElementById("totale").innerHTML = total;
+    }else {
         var article = null;
         var descr = null;
         var buttons = null;
@@ -51,21 +53,21 @@ function compute(xhttp) {
         var prezzo = null;
         var textPrezzo = null;
         var both = null;
-        for(i = 0;i < risp.articoli.length; i++){
+        for (i = 0; i < risp.articoli.length; i++) {
             article = document.createElement("div");
-            article.setAttribute("class","article");
+            article.setAttribute("class", "article");
             descr = document.createElement("div");
-            descr.setAttribute("class","info-article");
+            descr.setAttribute("class", "info-article");
             buttons = document.createElement("div");
-            buttons.setAttribute("class","button-article");
+            buttons.setAttribute("class", "button-article");
             editButton = document.createElement("button");
             deleteButton = document.createElement("button");
-            editButton.setAttribute("class","btn-edit");
+            editButton.setAttribute("class", "btn-edit");
             editButton.innerHTML = "MODIFICA";
-            deleteButton.setAttribute("class","btn-delete");
+            deleteButton.setAttribute("class", "btn-delete");
             deleteButton.innerHTML = "ELIMINA";
-            deleteButton.setAttribute("onclick","deleteArticle("+risp.articoli[i]["codice"]+")");
-            editButton.setAttribute("onclick","editArticle("+risp.articoli[i]["codice"]+")");
+            deleteButton.setAttribute("onclick", "deleteArticle(" + risp.articoli[i]["codice"] + ")");
+            editButton.setAttribute("onclick", "editArticle(" + risp.articoli[i]["codice"] + ")");
             buttons.appendChild(editButton);
             buttons.appendChild(document.createElement("br"));
             buttons.appendChild(deleteButton);
@@ -75,15 +77,17 @@ function compute(xhttp) {
             textH4 = document.createTextNode(risp.articoli[i]["nome"] + " (Qta: x" + risp.articoli[i]["quantita"] + ")");
             h4.appendChild(textH4);
             prezzo = document.createElement("span");
-            textPrezzo = document.createTextNode("P. parziale: " + (risp.articoli[i]["prezzo"]*risp.articoli[i]["quantita"]) + "EUR");
+            textPrezzo = document.createTextNode("P. parziale: " + (risp.articoli[i]["prezzo"] * risp.articoli[i]["quantita"]) + "EUR");
+            total += risp.articoli[i]["prezzo"] * risp.articoli[i]["quantita"];
             prezzo.appendChild(textPrezzo);
             both = document.createElement("div");
-            both.setAttribute("style","clear:both;");
+            both.setAttribute("style", "clear:both;");
             article.appendChild(both);
             descr.appendChild(h4);
             descr.appendChild(prezzo);
             div.appendChild(article);
         }
+        document.getElementById("totale").innerHTML = total;
     }
 }
 
