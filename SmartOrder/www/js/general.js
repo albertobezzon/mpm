4f972586983df3725f2ef106c22fc5e703e42f54
Tommaso Carraro <tommasocarraro@libero.it>
2019-01-27 20:31:07 +0100

    modifiche fighe

--- SmartOrder/www/js/general.js
+++ SmartOrder/www/js/general.js
@@ -1,8 +1,27 @@
+var urlString = window.location.href;
+var url = new URL(urlString);
+var codiceAzienda = url.searchParams.get("codiceAzienda");
+var username = url.searchParams.get("username");
+if(url.href.includes("homepage")){
+    document.getElementById("menu-cart").addEventListener("click",deleteMenu);
+    document.getElementById("menu-shop").setAttribute("href","inventory.html?codiceAzienda="+codiceAzienda+"&username="+username);
+    document.getElementById("menu-order").setAttribute("href","order.html?codiceAzienda="+codiceAzienda+"&username="+username);
+}else if(url.href.includes("inventory")){
+    document.getElementById("menu-cart").setAttribute("href","homepage.html?codiceAzienda="+codiceAzienda+"&username="+username)
+    document.getElementById("menu-shop").addEventListener("click",deleteMenu);
+    document.getElementById("menu-order").setAttribute("href","order.html?codiceAzienda="+codiceAzienda+"&username="+username);
+}else if(url.href.includes("order")){
+    document.getElementById("menu-cart").setAttribute("href","homepage.html?codiceAzienda="+codiceAzienda+"&username="+username)
+    document.getElementById("menu-shop").setAttribute("href","inventory.html?codiceAzienda="+codiceAzienda+"&username="+username);
+    document.getElementById("menu-order").addEventListener("click",deleteMenu);
+}
 var menuListener = document.getElementsByClassName("navbar-fostrap")[0];
 var containerListener = document.getElementsByClassName("body-container")[0];
 var footerListener = document.getElementsByClassName("banner-button")[0];
 menuListener.addEventListener("click",openMenu);
 containerListener.addEventListener("click",deleteMenu);
+document.getElementById("user").innerHTML = username;
+document.getElementById("company").innerHTML = codiceAzienda;
 if(footerListener != null){
     footerListener.addEventListener("click",deleteMenu);
 }
