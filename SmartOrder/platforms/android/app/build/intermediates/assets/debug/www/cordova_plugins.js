f41bacd321951300b8bd969601cfefbbc9bc5092
Tommaso Carraro <tommasocarraro@libero.it>
2019-01-29 19:02:19 +0100

    progetto android

---
+++ SmartOrder/platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
@@ -0,0 +1,36 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-dialogs.notification",
+    "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification_android",
+    "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+    "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+    "pluginId": "phonegap-plugin-barcodescanner",
+    "clobbers": [
+      "cordova.plugins.barcodeScanner"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-dialogs": "2.0.1",
+  "phonegap-plugin-barcodescanner": "8.0.1",
+  "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
