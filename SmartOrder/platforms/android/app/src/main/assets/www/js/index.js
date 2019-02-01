c2d3a5ca0a2f95d76eeed96312d899209cfbf938
Tommaso Carraro <tommasocarraro@libero.it>
2019-02-01 19:13:28 +0100

    sistemato device ready

--- SmartOrder/platforms/android/app/src/main/assets/www/js/index.js
+++ SmartOrder/platforms/android/app/src/main/assets/www/js/index.js
@@ -1,33 +1,19 @@
-document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     var username = "";
     var codiceAzienda = "";
-    function setPage() {
-        if(window.localStorage["username"] != null){
-            username = window.localStorage["username"];
-        }
-        if(window.localStorage["codiceAzienda"] != null){
-            codiceAzienda = window.localStorage["codiceAzienda"];
-        }
-        if(window.localStorage["loggedIn"] == null || window.localStorage["loggedIn"] == "false"){
-            location.replace("login.html");
-        }else{
-            location.replace("homepage.html?codiceAzienda="+codiceAzienda+"&username="+username)
-        }
+    if(window.localStorage["username"] != null){
+        username = window.localStorage["username"];
+    }
+    if(window.localStorage["codiceAzienda"] != null){
+        codiceAzienda = window.localStorage["codiceAzienda"];
+    }
+    if(window.localStorage["loggedIn"] == null || window.localStorage["loggedIn"] == "false"){
+        location.replace("login.html");
+    }else{
+        location.replace("homepage.html?codiceAzienda="+codiceAzienda+"&username="+username)
     }
 }
-var username = "";
-    var codiceAzienda = "";
-    function setPage() {
-        if(window.localStorage["username"] != null){
-            username = window.localStorage["username"];
-        }
-        if(window.localStorage["codiceAzienda"] != null){
-            codiceAzienda = window.localStorage["codiceAzienda"];
-        }
-        if(window.localStorage["loggedIn"] == null || window.localStorage["loggedIn"] == "false"){
-            location.replace("login.html");
-        }else{
-            location.replace("homepage.html?codiceAzienda="+codiceAzienda+"&username="+username)
-        }
-    }
+
+function setPage() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
